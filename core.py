1e7d93a3e2eeb229fecedcd0f6f503a56f2f62cd
Saulo R.S. <165298753+Saul0RS@users.noreply.github.com>
2026-05-19 19:14:46 -0300

    Add files via upload

---
+++ core.py
@@ -0,0 +1,21 @@
+from defs import base
+
+
+
+
+while True:
+    opcao = base.menu()
+
+    if opcao == 0:
+        break
+    elif opcao== 1:
+        print("opcao 1 IDENTIFICADA")
+    elif opcao == 2:
+        print("opcao 2 IDENTIFICADA")
+    elif opcao == 3:
+        print("opcao 3 IDENTIFICADA")
+    elif opcao == 4:
+        print("opcao 4 IDENTIFICADA")
+    else:
+        print("OPÇÃO INVALIDA !!!")
+        
---
+++ defs/base.py
@@ -0,0 +1,22 @@
+def entrada_menu():
+
+    print("""
+          
+    1 - adicionar evento
+    2 - visualizar
+    3 - editar
+    4 - excluir
+    0 - sair
+        """)
+    
+    opcao = int(input("Digite a opcao: "))
+
+    return opcao
+
+def add_user():
+
+    nome = input("Digite seu nome: ")
+    senha = input("Digite sua senha: ")
+
+    
+
