7dd10ce5fb95c2bc99bdb5b12f89b1baa1439f34
Saul0RS <srs.oficial2023@gmail.com>
2026-05-20 22:47:58 -0300

    função login() aprimorada e inicio da função esqueci_senha()

--- defs/base.py
+++ defs/base.py
@@ -1,3 +1,4 @@
+import os 
 
 def show_options(options):
 
@@ -14,6 +15,8 @@ def show_options(options):
 def initial_menu():
 
     opcao = show_options(["Cadastrar", "Entrar", "Visualizar Eventos", "Sair"])
+    
+    os.system("cls")
     if opcao == 1:
         add_user()
     elif opcao == 2:
@@ -28,18 +31,47 @@ def add_user():
 
     nome = input("Digite seu nome: ")
     senha = input("Digite sua senha: ")
-
-    with open("database/users.txt", "a") as file:
+    #Colocar perguntas e respostas de segurança aqui para recuperação de senha no login()  
+    with open("database/users.txt", "a", encoding="utf-8") as file:
         file.write(f"\"nome\": \"{nome}\", \"senha\": \"{senha}\"\n")
 
 def login():
-
+    #while True
     nome = input("Digite seu nome: ")
     senha = input("Digite sua senha: ")
-
-    with open("database/users.txt", "r") as file:
+    with open("database/users.txt", "r", encoding="utf-8") as file:
         if f"\"nome\": \"{nome}\", \"senha\": \"{senha}\"" in file.read():
             print("Login bem-sucedido!")
             return True
-    print("Verifique suas credenciais.")
-    return False
+        else: 
+            print("Login incorreto !!!")
+            n = input("Deseja recuperar sua senha? Sim ou Não?").upper()
+            os.system("cls")
+            if n == 'S' or n == "SIM":
+                esqueci_senha()
+                os.system("cls")
+                print("Tente o login novamente com a nova senha !!!")
+                #continue pro while True aqui
+            elif n == 'N' or n == "NÃO" or n == "NAO":
+                return False
+            else:
+                os.system("cls")
+                print("Tente o login novamente novamente !!!")
+                #continue pro while True aqui
+
+
+
+def esqueci_senha():
+    nome = input("Digite seu nome: ").capitalize()
+    with open("database/users.txt", "r", encoding="utf-8") as file:
+        for linha in file:
+            if f"\"nome\": \"{nome}\"" in linha:
+                inicio = linha.find('"question":')
+                fim = linha.find(",", inicio)
+                pergunta = linha[inicio:fim]
+                print(pergunta)
+                return pergunta
+            else:
+                return print("n deu certo !!!")
+                
+            
