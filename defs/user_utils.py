04eda43124295f1f5fc2c7b00f6ef653387687a1
Idinaldo <idinaldooliveira220@gmail.com>
2026-05-26 02:35:48 -0300

    feat: add find_me, update_user and delete_user

--- defs/user_utils.py
+++ defs/user_utils.py
@@ -1,5 +1,6 @@
-from utils import show_title
+from defs.utils import show_title
 
+# CREATE
 def add_user():
     show_title("Criar Conta")    
     name = input("Digite seu nome: ")
@@ -17,6 +18,7 @@ def add_user():
         print("Usuário cadastrado com sucesso!")
         return 0
 
+
 def login():
     show_title("Login")
     email = input("Digite seu email: ")
@@ -29,3 +31,39 @@ def login():
         else:
             print("Login incorreto tente novamente !!!")
             return False
+
+
+# READ
+def find_me(email):
+    with open("database/users.txt", "r") as file:
+        for line in file:
+            if f"\"email\": \"{email}\"" in line:
+                return line
+
+
+# UPDATE
+def update_user(email):
+    show_title("Atualizar Dados")
+    name = input("Digite seu nome: ")
+    password = input("Digite sua senha: ")
+
+    with open("database/users.txt", "r") as file:
+        lines = file.readlines()
+
+    with open("database/users.txt", "w") as file:
+        for line in lines:
+            if f"\"email\": \"{email}\"" in line:
+                file.write(f"{{\"nome\": \"{name}\", \"email\": \"{email}\", \"senha\": \"{password}\"}}\n")
+            else:
+                file.write(line)
+
+
+# DELETE
+def delete_user(email):
+    with open("database/users.txt", "r") as file:
+        lines = file.readlines()
+
+    with open("database/users.txt", "w") as file:
+        for line in lines:
+            if f"\"email\": \"{email}\"" not in line:
+                file.write(line)
